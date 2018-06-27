3a6e9710f61c45b007bbbe21dfcd1945a025d67f
piyawat37 <piyawat.pem.it57@cpc.ac.th>
2018-06-27 09:04:42 +0700

    Initial

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <meta charset="utf-8">
+    <title>Master Page</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+      @include('inc.navbar')
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-lg-8">
+            @yield('content')
+          </div>
+          <div class="col-md-4 col-lg-4">
+            @include('inc.sidebar')
+          </div>
+        </div>
+      </div>
+  </body>
+</html>
